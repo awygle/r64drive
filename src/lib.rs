8e7de551439b87c97b7ed6464432d6c1cb82ab04
Andrew Wygle <awygle@gmail.com>
2019-05-01 15:53:57 -0700

    Sketch of constructor and send_cmd function.
    Tests still fail.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,36 @@
+use byteorder::{BigEndian, ByteOrder};
+use safe_ftdi as ftdi;
 
 pub enum Commands {
     VersionRequest = 0x80,
 }
 
 pub struct R64Drive {
-    
+    context: ftdi::Context,
 }
 
 impl R64Drive {
     pub fn send_cmd(&self, cmd_id: Commands, args: &[u32]) {
-        
+        let mut buf: Vec<u8> = Vec::with_capacity((args.len() + 1) * 4);
+        BigEndian::write_u32(&mut buf, cmd_id as u32);
+        for (i, arg) in args.iter().cloned().enumerate() {
+            BigEndian::write_u32(&mut buf[i * 4..], arg);
+        }
+        self.context.write_data(&buf).unwrap();
     }
-    
+
     pub fn new() -> R64Drive {
-        R64Drive { }
+        // TODO take a hardware version or VID/PID
+        let mut result = R64Drive {
+            context: ftdi::Context::new().unwrap(),
+        };
+        result.context.open(0x0403, 0x6014).unwrap();
+        result
     }
 }
 
+impl Default for R64Drive {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- tests/api.rs
+++ tests/api.rs
@@ -4,7 +4,7 @@ mod tests {
     #[test]
     fn test_send_cmd() {
         let cmd_id = Commands::VersionRequest;
-        
+
         let r64d = R64Drive::new();
         r64d.send_cmd(cmd_id, &[]);
     }
