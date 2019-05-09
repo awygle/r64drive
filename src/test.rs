c87214d3cfef9cd9dcba096d52937df5bf727691
Andrew Wygle <awygle@gmail.com>
2019-05-09 15:56:20 -0700

    Separate out the mock from the driver in test impl

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::cell::RefCell;
 
 #[derive(Copy, Clone, Debug)]
 enum States {
@@ -7,17 +8,25 @@ enum States {
     Finished,
 }
 
-pub struct R64DriveTest {
-    state: std::cell::Cell<States>,
+#[derive(Copy, Clone, Debug)]
+struct R64DriveTest {
+    state: States,
 }
 
-impl<'a> R64Driver<'a> for R64DriveTest {
-    type Error = (&'static str, u32);
-    fn send_u32(&'a self, val: u32) -> Result<usize, Self::Error> {
-        match self.state.get() {
+impl Default for R64DriveTest {
+    fn default() -> Self {
+        R64DriveTest {
+            state: States::Idle,
+        }
+    }
+}
+
+impl R64DriveTest {
+    fn recv_u32(&mut self, val: u32) -> Result<usize, (&'static str, u32)> {
+        match self.state {
             States::Idle => match val >> 24 {
                 0x80 => {
-                    self.state.set(States::VersionRequest);
+                    self.state = States::VersionRequest;
                     Ok(4)
                 }
                 _ => Err(("invalid command in state Idle", val)),
@@ -27,11 +36,11 @@ impl<'a> R64Driver<'a> for R64DriveTest {
         }
     }
 
-    fn recv_u32(&'a self) -> Result<u32, Self::Error> {
-        match self.state.get() {
+    fn send_u32(&mut self) -> Result<u32, (&'static str, u32)> {
+        match self.state {
             States::Idle => Err(("unexpected read in state Idle", 0)),
             States::VersionRequest => {
-                self.state.set(States::Finished);
+                self.state = States::Finished;
                 Ok(0x4200_00CD)
             }
             States::Finished => Ok(0x43_4D_50_80u32),
@@ -39,15 +48,32 @@ impl<'a> R64Driver<'a> for R64DriveTest {
     }
 }
 
-impl R64DriveTest {
-    pub fn new() -> R64DriveTest {
-        R64DriveTest {
-            state: std::cell::Cell::new(States::Idle),
+pub struct R64DriverTest {
+    mock: RefCell<R64DriveTest>,
+}
+
+impl<'a> R64Driver<'a> for R64DriverTest {
+    type Error = (&'static str, u32);
+    fn send_u32(&'a self, val: u32) -> Result<usize, Self::Error> {
+        self.mock.borrow_mut().recv_u32(val)
+    }
+
+    fn recv_u32(&'a self) -> Result<u32, Self::Error> {
+        self.mock.borrow_mut().send_u32()
+    }
+}
+
+impl R64DriverTest {
+    pub fn new() -> R64DriverTest {
+        R64DriverTest {
+            mock: RefCell::new(R64DriveTest {
+                state: States::Idle,
+            }),
         }
     }
 }
 
-impl Default for R64DriveTest {
+impl Default for R64DriverTest {
     fn default() -> Self {
         Self::new()
     }
--- tests/api.rs
+++ tests/api.rs
@@ -3,7 +3,7 @@ mod tests {
     use r64drive::*;
     #[test]
     fn test_get_version() {
-        let driver = test::R64DriveTest::new();
+        let driver = test::R64DriverTest::new();
         let r64d = R64Drive::new(&driver);
         r64d.get_version().unwrap();
     }
