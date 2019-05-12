a4d2a42aa9b32239f1d6e456ad822496ca1793a0
awygle <awygle@gmail.com>
2019-05-11 20:27:00 -0700

    Actually check result of Get Version test

--- tests/api.rs
+++ tests/api.rs
@@ -5,6 +5,8 @@ mod tests {
     fn test_get_version() {
         let driver = test::R64DriverTest::new();
         let r64d = R64Drive::new(&driver);
-        r64d.get_version().unwrap();
+        let (variant, version) = r64d.get_version().unwrap();
+        assert!(variant == 0x4200);
+        assert!(version == 0x00CD);
     }
 }
